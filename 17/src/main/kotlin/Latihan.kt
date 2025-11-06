87bdd44a65964cef101ade23c5f70b5cbdb421a0
DanielWijayaa <danielwijaya_2226250060@mhs.mdp.ac.id>
2025-11-06 20:27:22 +0700

    pertemuan 17

--- 17/src/main/kotlin/Latihan.kt
+++ 17/src/main/kotlin/Latihan.kt
@@ -25,4 +25,15 @@ open class Programmer(nama: String,Gaji_dasar: Double): Pegawai(nama,Gaji_dasar)
 fun main() {
     Programmer("DanielWijaya",90000.00).hitungGaji()
     Manager("Lazu",1000000000.00).hitungGaji()
+    val list=mutableListOf<Pegawai>(Programmer("DanielWijaya",2000.00), Programmer("Lazu",100.00))
+    val list1=mutableListOf<Programmer>(Programmer("DanielWijaya",2000.00), Programmer("Lazu",100.00))
+    for(a in list)
+    {
+        a.hitungGaji()
+    }
+    for(b in list1)
+    {
+        b.hitungGaji()
+    }
+
 }
